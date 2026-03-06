24fbaefb801bb271b2b963bd46461647f74bb789
ganji <ganjipraveen444@gmail.com>
2026-03-06 15:51:51 +0530

    corrected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 
-import connectDB from './src/database/connectDB'
+import connectDB from './src/database/connectDB.js'
 import app from './src/app.js'
 import dotenv from 'dotenv'
 dotenv.config();
