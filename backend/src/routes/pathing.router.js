b636f84bab067cf7c31e87e7d6085b4d5a202e6c
ganji <ganjipraveen444@gmail.com>
2026-03-06 16:45:15 +0530

    get is done

--- backend/src/routes/pathing.router.js
+++ backend/src/routes/pathing.router.js
@@ -3,7 +3,7 @@ import { welcome } from "../controller/welcome.js";
 
 
 const router = Router();
-router.route('/welcome').post(welcome);  //https://dsa-sheets-jgbg.onrender.com/api/dsa-sheets/welcome
+router.route('/').get(welcome);  //https://dsa-sheets-jgbg.onrender.com/api/dsa-sheets/welcome
 
 
 export default router;
